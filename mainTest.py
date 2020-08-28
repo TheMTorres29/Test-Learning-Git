5ac21ad010ba9a8f77453905ea5cdd15ac9103bf
TheMTorres29 <39773958+TheMTorres29@users.noreply.github.com>
2020-08-27 17:15:05 -0700

    Added python file for first time

---
+++ mainTest.py
@@ -0,0 +1,5 @@
+# This is a test to see the changes in a python file through pushing and pulling
+# Im using python as a test b/c I have never done js or html before.
+
+# First piece of code
+print("Hello all.")
